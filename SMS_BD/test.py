9599c6715968320b700359c7d97a0f43e4dea761
neon520 <javierpg520@gmail.com>
2015-10-30 18:34:56 +0100

    rehaciendo tests

--- SMS_BD/test.py
+++ SMS_BD/test.py
@@ -3,40 +3,85 @@ import webapp2
 import gestorhtml
 
 class TestStringMethods(unittest.TestCase):
-        def test_empresas(self):
-            request = webapp2.Request.blank('/empresas')
+        def test_alumnos(self):
+            request = webapp2.Request.blank('/alumnos')
             response = request.get_response(gestorhtml.Server())
-            self.assertTrue('Empresas registradas' in response.body)
-
+            self.assertTrue('Alumnos registrados' in response.body)
+'''
         def test_borrarCalificacion(self):
             request = webapp2.Request.blank('/borrarCalificacion')
             response = request.get_response(gestorhtml.Server())
             self.assertTrue('Borrado de una Calific' in response.body)
-
-        def test_calificaciones(self):
-            request = webapp2.Request.blank('/calificaciones')
+'''
+        def test_asignaturas(self):
+            request = webapp2.Request.blank('/asignaturas')
             response = request.get_response(gestorhtml.Server())
-            self.assertTrue('Calificaciones registradas' in response.body)
+            self.assertTrue('Asignaturas registradas' in response.body)
 
-        def test_registroCalificar(self):
-            request = webapp2.Request.blank('/registroCalificar')
+        def test_cursos(self):
+            request = webapp2.Request.blank('/cursos')
             response = request.get_response(gestorhtml.Server())
-            self.assertTrue('Registro de una Calific' in response.body)
+            self.assertTrue('Cursos registrados' in response.body)
 
-        def test_registroEmpresa(self):
-            request = webapp2.Request.blank('/registroEmpresa')
+        def test_grupos(self):
+            request = webapp2.Request.blank('/grupos')
             response = request.get_response(gestorhtml.Server())
-            self.assertTrue('Registro de la empresa' in response.body)
+            self.assertTrue('Grupos registrados' in response.body)
 
-        def test_registroUsuario(self):
-            request = webapp2.Request.blank('/registroUsuario')
+        def test_imparticiones(self):
+            request = webapp2.Request.blank('/imparticiones')
+            response = request.get_response(gestorhtml.Server())
+            self.assertTrue('Imparticiones registradas' in response.body)
+
+        def test_pertenencias(self):
+            request = webapp2.Request.blank('/pertenencias')
+            response = request.get_response(gestorhtml.Server())
+            self.assertTrue('Pertenencias registradas' in response.body)
+
+        def test_profesores(self):
+            request = webapp2.Request.blank('/profesores')
+            response = request.get_response(gestorhtml.Server())
+            self.assertTrue('Profesores registrados' in response.body)
+
+        def test_registroAlumno(self):
+            request = webapp2.Request.blank('/registroAlumno')
             response = request.get_response(gestorhtml.Server())
             self.assertTrue('Registro de alumno' in response.body)
+
+        def test_registroAsignatura(self):
+            request = webapp2.Request.blank('/registroAsignatura')
+            response = request.get_response(gestorhtml.Server())
+            self.assertTrue('Registro de asignatura' in response.body)
+
+        def test_registroCursa(self):
+            request = webapp2.Request.blank('/registroCursa')
+            response = request.get_response(gestorhtml.Server())
+            self.assertTrue('Registro de cursar' in response.body)
+
+        def test_registroGrupo(self):
+            request = webapp2.Request.blank('/registroGrupo')
+            response = request.get_response(gestorhtml.Server())
+            self.assertTrue('Registro de grupo' in response.body)
+
+        def test_registroImparte(self):
+            request = webapp2.Request.blank('/registroImparte')
+            response = request.get_response(gestorhtml.Server())
+            self.assertTrue('Registro de imparticiones' in response.body)
+
+        def test_registroPertenece(self):
+            request = webapp2.Request.blank('/registroPertenece')
+            response = request.get_response(gestorhtml.Server())
+            self.assertTrue('Registro de pertenece' in response.body)
+
+        def test_registroProfesor(self):
+            request = webapp2.Request.blank('/registroProfesor')
+            response = request.get_response(gestorhtml.Server())
+            self.assertTrue('Registro de Profesor' in response.body)
 
         def test_base(self):
             request = webapp2.Request.blank('/')
             response = request.get_response(gestorhtml.Server())
-            self.assertTrue('n de empresas' in response.body)
+            self.assertTrue('Students Management System' in response.body)
 
 if __name__ == '__main__':
     unittest.main()
--- SMS_BD/test_sure.py
+++ SMS_BD/test_sure.py
@@ -3,45 +3,100 @@ import webapp2
 import gestorhtml
 
 class TestStringMethods:
-        def test_empresas(self):
-            request = webapp2.Request.blank('/empresas')
-            response = request.get_response(gestorhtml.Server())
-            #self.assertTrue('Empresas registradas' in response.body)
-            (response.body).should.contain('Empresas registradas')
 
+        def test_alumnos(self):
+            request = webapp2.Request.blank('/alumnos')
+            response = request.get_response(gestorhtml.Server())
+            #self.assertTrue('Alumnos registrados' in response.body)
+            (response.body).should.contain('Alumnos registrados')
+'''
         def test_borrarCalificacion(self):
             request = webapp2.Request.blank('/borrarCalificacion')
             response = request.get_response(gestorhtml.Server())
-            #self.assertTrue('Borrado de una Calific' in response.body)
-            (response.body).should.contain('Borrado de una Calific')
-
-        def test_calificaciones(self):
-            request = webapp2.Request.blank('/calificaciones')
+            self.assertTrue('Borrado de una Calific' in response.body)
+'''
+        def test_asignaturas(self):
+            request = webapp2.Request.blank('/asignaturas')
             response = request.get_response(gestorhtml.Server())
-            #self.assertTrue('Calificaciones registradas' in response.body)
-            (response.body).should.contain('Calificaciones registradas')
+            #self.assertTrue('Asignaturas registradas' in response.body)
+            (response.body).should.contain('Asignaturas registradas')
 
-        def test_registroCalificar(self):
-            request = webapp2.Request.blank('/registroCalificar')
+        def test_cursos(self):
+            request = webapp2.Request.blank('/cursos')
             response = request.get_response(gestorhtml.Server())
-            #self.assertTrue('Registro de una Calific' in response.body)
-            (response.body).should.contain('Registro de una Calific')
+            #self.assertTrue('Cursos registrados' in response.body)
+            (response.body).should.contain('Cursos registrados')
 
-        def test_registroEmpresa(self):
-            request = webapp2.Request.blank('/registroEmpresa')
+        def test_grupos(self):
+            request = webapp2.Request.blank('/grupos')
             response = request.get_response(gestorhtml.Server())
-            #self.assertTrue('Registro de la empresa' in response.body)
-            (response.body).should.contain('Registro de la empresa')
+            #self.assertTrue('Grupos registrados' in response.body)
+            (response.body).should.contain('Grupos registrados')
 
-        def test_registroUsuario(self):
-            request = webapp2.Request.blank('/registroUsuario')
+        def test_imparticiones(self):
+            request = webapp2.Request.blank('/imparticiones')
+            response = request.get_response(gestorhtml.Server())
+            #self.assertTrue('Imparticiones registradas' in response.body)
+            (response.body).should.contain('Imparticiones registradas')
+
+        def test_pertenencias(self):
+            request = webapp2.Request.blank('/pertenencias')
+            response = request.get_response(gestorhtml.Server())
+            #self.assertTrue('Pertenencias registradas' in response.body)
+            (response.body).should.contain('Pertenencias registradas')
+
+        def test_profesores(self):
+            request = webapp2.Request.blank('/profesores')
+            response = request.get_response(gestorhtml.Server())
+            #self.assertTrue('Profesores registrados' in response.body)
+            (response.body).should.contain('Profesores registrados')
+
+        def test_registroAlumno(self):
+            request = webapp2.Request.blank('/registroAlumno')
             response = request.get_response(gestorhtml.Server())
             #self.assertTrue('Registro de alumno' in response.body)
             (response.body).should.contain('Registro de alumno')
 
+        def test_registroAsignatura(self):
+            request = webapp2.Request.blank('/registroAsignatura')
+            response = request.get_response(gestorhtml.Server())
+            #self.assertTrue('Registro de asignatura' in response.body)
+            (response.body).should.contain('Registro de asignatura')
+
+        def test_registroCursa(self):
+            request = webapp2.Request.blank('/registroCursa')
+            response = request.get_response(gestorhtml.Server())
+            #self.assertTrue('Registro de cursar' in response.body)
+            (response.body).should.contain('Registro de cursar')
+
+        def test_registroGrupo(self):
+            request = webapp2.Request.blank('/registroGrupo')
+            response = request.get_response(gestorhtml.Server())
+            #self.assertTrue('Registro de grupo' in response.body)
+            (response.body).should.contain('Registro de grupo')
+
+        def test_registroImparte(self):
+            request = webapp2.Request.blank('/registroImparte')
+            response = request.get_response(gestorhtml.Server())
+            #self.assertTrue('Registro de imparticiones' in response.body)
+            (response.body).should.contain('Registro de imparticiones')
+
+        def test_registroPertenece(self):
+            request = webapp2.Request.blank('/registroPertenece')
+            response = request.get_response(gestorhtml.Server())
+            #self.assertTrue('Registro de pertenece' in response.body)
+            (response.body).should.contain('Registro de pertenece')
+
+        def test_registroProfesor(self):
+            request = webapp2.Request.blank('/registroProfesor')
+            response = request.get_response(gestorhtml.Server())
+            #self.assertTrue('Registro de Profesor' in response.body)
+            (response.body).should.contain('Registro de Profesor')
+
+
         def test_base(self):
             request = webapp2.Request.blank('/')
             response = request.get_response(gestorhtml.Server())
-            #self.assertTrue('n de empresas' in response.body)
-            (response.body).should.contain('n de empresas')
+            #self.assertTrue('Students Management System' in response.body)
+            (response.body).should.contain('Students Management System')
 
