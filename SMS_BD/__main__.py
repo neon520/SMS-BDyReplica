812674361f96e96f12b81a428ebf0a71c269e5ce
neon520 <javierpg520@gmail.com>
2015-11-04 13:21:03 +0100

    modificando el main

--- SMS_BD/__main__.py
+++ SMS_BD/__main__.py
@@ -8,7 +8,7 @@ from gestorhtml import *
 def main():
     from paste import httpserver
     app = Server()
-    httpserver.serve(app, host='127.0.0.1', port='8000')
+    ap=httpserver.serve(app, host='127.0.0.1', port='8000')
 
 if __name__ == '__main__':
     main()
